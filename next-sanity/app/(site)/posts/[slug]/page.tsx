50e14980f9c939583e9340a873080f5d26cc752b
Greg Parr <greg.parr@blueyonder.co.uk>
2024-02-20 22:30:28 +0000

    handled missing date on blog posts

--- next-sanity/app/(site)/posts/[slug]/page.tsx
+++ next-sanity/app/(site)/posts/[slug]/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({ params }: Props) {
             <h1 className="text-5xl drop-shadow font-extrabold bg-gradient-to-r from-blue-500 to-fuchsia-600
                 bg-clip-text text-transparent">{post.title}</h1>
 
-            <p className="text-md text-gray-200 mt-10 font-bold">Published: {new Date(post.publishedAt.toString()).toLocaleDateString('en-GB')}</p>
+            <p className="text-md text-gray-200 mt-10 font-bold">Published: {new Date(post.publishedAt?.toString()).toLocaleDateString('en-GB')}</p>
       
             <div className="text-lg text-gray-200 mt-10 space-y-8">
                 <BlockContent content={post.content}></BlockContent>
